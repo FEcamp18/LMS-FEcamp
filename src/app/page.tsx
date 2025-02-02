dcacac880a4cd0482d39d7df3b76bdd2c16bc372
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-02-02 13:27:44 +0700

    feat: change phase button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,63 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
-  const WebPhase: string = "BeforeCam"
-  const router = useRouter()
+  const [webPhase, setWebPhase] = useState<string>("Open");
+  const router = useRouter();
 
   function redirectToLogin() {
-    router.push("/login")
+    router.push("/login");
   }
-
-  function redirectToClassroom() {
-
-    router.push("/classroom")
-  }
-
-  function redirectToFeedback() {
-    router.push("/feedback")
+  function changePhase(phase: string) {
+    setWebPhase(phase);
   }
 
   return (
     <div>
-      {(WebPhase === "BeforeCamp" || WebPhase !== "Close") && (
-        <div className="bg-gray-300 w-full h-16 flex justify-between items-center px-6 py-2 fixed top-0 left-0 z-10">
-          
-          <img
-            src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"
-            className="w-12 h-12 rounded-full"
-          />
-          <div className="flex justify-end items-center space-x-4">
-            <button
-              onClick={redirectToClassroom}
-              className="bg-grey-500 text-black rounded-full px-6 py-2"
-            >
-              Classroom
-            </button>
-            <button
-              onClick={redirectToFeedback}
-              className="bg-grey-500 text-black rounded-full px-6 py-2"
-            >
-              Feedback
-            </button>
-          </div>
-        </div>
-      )}
-
-      <div className="flex justify-center items-center min-h-screen pt-16">
-
-        {WebPhase === "Close" ? (
+      <div className="flex min-h-screen flex-col items-center justify-center pt-16">
+        {webPhase === "Close" ? (
           <div className="text-4xl font-bold">ปิดปรับปรุง</div>
-        ) : WebPhase === "BeforeCamp" ? (
+        ) : webPhase === "BeforeCamp" ? (
           <div className="text-center">
-            <h1 className="text-3xl font-semibold mb-4">Landing Page ngub</h1>
-            <button
-              className="px-8 py-3 bg-gray-300 text-white rounded-full border-2 border-gray-400 cursor-not-allowed"
-            >
+            <h1 className="mb-4 text-3xl font-semibold">Landing Page ngub</h1>
+            <Button className="cursor-not-allowed rounded-full border-2 border-gray-400 bg-gray-300 px-8 py-3 text-white hover:bg-gray-400">
               Coming Soon
-            </button>
+            </Button>
           </div>
         ) : (
-          
           <div className="text-center">
-            <h1 className="text-3xl font-semibold mb-4">Landing Page ngub</h1>
-            <button onClick={redirectToLogin} className="px-8 py-3 bg-blue-500 text-white rounded-full">
-              Login</button>
+            <h1 className="mb-4 text-3xl font-semibold">Landing Page ngub</h1>
+            <Button
+              onClick={redirectToLogin}
+              className="rounded-full bg-blue-500 px-8 py-3 text-white hover:bg-blue-400"
+            >
+              Login
+            </Button>
           </div>
         )}
+        <div className="mt-24 flex gap-4">
+          <Button
+            onClick={() => changePhase("BeforeCamp")}
+            className="w-32 bg-yellow-500 hover:bg-yellow-400"
+          >
+            Before Camp
+          </Button>
+          <Button
+            onClick={() => changePhase("Open")}
+            className="w-32 bg-green-500 hover:bg-green-400"
+          >
+            Open
+          </Button>
+          <Button
+            onClick={() => changePhase("Close")}
+            className="w-32 bg-red-500 hover:bg-red-400"
+          >
+            Closing
+          </Button>
+        </div>
       </div>
     </div>
   );
