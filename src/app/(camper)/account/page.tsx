e99499a194b8023f08170b528249d7b2065094be
Wachirawit Chumwong <punwachiza@gmail.com>
2025-02-06 21:33:46 +0700

    feat: แก้กับอัพเดท
    แก้ tag ของ task 1 กับ task 2
    ทำ task 3 กับ 4 เพิ่ม
    style ยังไม่ได้ตกแต่งตาม figma ขนาดนั้น

--- src/app/(camper)/account/page.tsx
+++ src/app/(camper)/account/page.tsx
@@ -1,22 +1,23 @@
 // write your code here
-import { type Metadata } from "next";
+import Logout from "./Logout"
+import ChangePassForm from "./ChangePass"
 
-export const metadata: Metadata = {
-  title: "Account page | FE Camp"
-};
 
 export default function AccountPage() {
-  
+
+  const logout = () => {
+    console.log('log out')
+  }
 
   return (
     <>
-      <div className='mx-8 mt-20 text-4xl'>
+      <h1 className='mx-8 mt-20 text-4xl'>
         นี่ชื่อน้องค่าย นี่นามสกุล (นี่ชื่อเล่น)
-      </div>
+      </h1>
 
-      <div className='md:grid grid-cols-6 gap-8 mt-5 mx-9'>
+      <main className='md:grid grid-cols-6 gap-8 mt-5 mx-9'>
         
-        <div className='col-span-2 text-xl'>
+        <section className='col-span-2 text-xl'>
           <h1 className='font-semibold mb-1'>ข้อมูลส่วนตัว</h1>
           <ul>
             <li> &#8226; ContactTel</li>
@@ -25,41 +26,36 @@ export default function AccountPage() {
             <li> &#8226; parentTel (ParentRalationship)</li>
             <li> &#8226; Section</li>
           </ul>
-        </div>
+        </section>
 
-        <div className='col-span-3 text-xl'>
+        <section className='col-span-3 text-xl'>
           
           <div>
-            <h1 className='font-semibold ml-10'>HealthInfo</h1>
-            <div className='ml-10 py-10 mt-4 bg-slate-200 text-center'>
+            <h2 className='font-semibold ml-10'>HealthInfo</h2>
+            <p className='ml-10 py-10 mt-4 bg-slate-200 text-center'>
               textarea ยาวๆ
-            </div>
+            </p>
           </div>
 
           <div className='mt-5'>
-            <h1 className='font-semibold ml-10'>FoodInfo</h1>
-            <div className='py-10 mt-4 ml-10 bg-slate-200 text-center'>
+            <h2 className='font-semibold ml-10'>FoodInfo</h2>
+            <p className='py-10 mt-4 ml-10 bg-slate-200 text-center'>
               textarea ยาวๆ
-            </div>
+            </p>
           </div>
       
-        </div>  
+        </section>  
 
-      </div>
+      </main>
 
       <div className='mx-9 mt-8'>
-        <button className='bg-slate-300 rounded-3xl py-3 px-16 text-lg ' >
-          change password
-        </button>
-
-        <button className='bg-slate-300 rounded-3xl py-3 px-28 mt-4 text-lg block'>
-          log out
-        </button>
+        <Logout/>
+        <ChangePassForm/>
       </div>
-      
-    
-    
+
     </>
   )
 }
+
+
 
