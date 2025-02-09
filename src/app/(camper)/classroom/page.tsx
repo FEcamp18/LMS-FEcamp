09349809fa2ac019a79a9d2c0bd383180aa30d3e
Sushi <kaopansri@gmail.com>
2025-02-09 22:11:51 +0700

    kaopan/feat/announcementCard

--- src/app/(camper)/classroom/page.tsx
+++ src/app/(camper)/classroom/page.tsx
@@ -1,1 +1,13 @@
 // write your code here
+import AnnouncementCard from '@/components/ui/classroom/announcementCard'
+
+
+export default function page() {
+  return (
+    <AnnouncementCard 
+      annoTitle="หัวข้อประกาศสั้นๆ"
+      annoText="ข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆ....." 
+      annoTime= {new Date("2568-06-10 10:10:00")}
+      />
+  )
+}
