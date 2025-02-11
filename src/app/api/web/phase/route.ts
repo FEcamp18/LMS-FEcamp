0035255b5b9a6eaa52615b9869913e0706581026
098ff <chanatdakc@gmail.com>
2025-02-11 22:33:06 +0700

    feat: done for api/web/phase/route.ts

---
+++ src/app/api/web/phase/route.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function GET() {
+    try {
+        const phase = await prisma.webPhase.findFirst();
+
+        if (!phase) { // if phase === null value
+            return Response.json(
+                {
+                    "message": "failed",
+                    "error": "No phase data available.",
+                },
+                {
+                    status: 404,
+                },
+            );
+        }
+
+        return Response.json(
+            {
+                message: "success",
+                phase: "CAMP",
+            },
+            {
+                status: 200,
+            },
+        );
+    }
+    catch (error) {
+        return Response.json(
+            {
+                message: "failed",
+                error: error,
+            },
+            {
+                status: 500,
+            },
+        );
+    }
+}
