ecc124024126f488489ae84ab15d944c1a923a21
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-02-13 16:19:50 +0700

    fix: change tag `h1` to `h3` in Announcement card

--- src/app/(camper)/classroom/page.tsx
+++ src/app/(camper)/classroom/page.tsx
@@ -1,13 +1,12 @@
 // write your code here
-import AnnouncementCard from '@/components/ui/classroom/announcementCard'
-
+import AnnouncementCard from "@/components/classroom/announcementCard";
 
 export default function page() {
   return (
-    <AnnouncementCard 
+    <AnnouncementCard
       annoTitle="หัวข้อประกาศสั้นๆ"
-      annoText="ข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆ....." 
-      annoTime= {new Date("2568-06-10 10:10:00")}
-      />
-  )
+      annoText="ข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆข้อความยาวๆ....."
+      annoTime={new Date("2568-06-10 10:10:00")}
+    />
+  );
 }
--- src/components/classroom/announcementCard.tsx
+++ src/components/classroom/announcementCard.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 type announcementCard = {
-    annoTime:Date, 
-    annoTitle:string, 
-    annoText:string, 
-}
+  annoTime: Date;
+  annoTitle: string;
+  annoText: string;
+};
 
-export default function AnnouncementCard({annoTime,annoTitle,annoText}: announcementCard) {
-    return (
-        <div className= "max-w-2xl max-h-30 rounded-2xl grid grid-cols-5 gap-3 p-3 pl-5 m-3 border-2 border-black bg-gray-100">
-            <div className="border-b sm:border-b-0 sm:border-r-4 border-gray-400">
-                <p className= "align-middle p-2 text-gray-600">
-                    {`${annoTime.getDate()}/${annoTime.getMonth()+1}/${annoTime.getFullYear().toString().substr(-2)}`} <br />  {`${annoTime.getHours()}:${annoTime.getMinutes()}` }
-                </p>
-            </div>
-            <div className='col-span-4'>
-                <h1 className= "font-bold">
-                    {annoTitle} 
-                </h1>
-                <p className= "font-light text-sm">
-                    {annoText}
-                </p>
-            </div>
-            
-        </div>
-    )
+export default function AnnouncementCard({
+  annoTime,
+  annoTitle,
+  annoText,
+}: announcementCard) {
+  return (
+    <div className="max-h-30 m-3 grid max-w-2xl grid-cols-5 gap-3 rounded-2xl border-2 border-black bg-gray-100 p-3 pl-5">
+      <div className="border-b border-gray-400 sm:border-b-0 sm:border-r-4">
+        <p className="p-2 align-middle text-gray-600">
+          {`${annoTime.getDate()}/${annoTime.getMonth() + 1}/${annoTime.getFullYear().toString().substr(-2)}`}{" "}
+          <br /> {`${annoTime.getHours()}:${annoTime.getMinutes()}`}
+        </p>
+      </div>
+      <div className="col-span-4">
+        <h3 className="font-bold">{annoTitle}</h3>
+        <p className="text-sm font-light">{annoText}</p>
+      </div>
+    </div>
+  );
 }
