7e6cf60499929938231219a6a35d245c3a6d80ac
Wachirawit Chumwong <punwachiza@gmail.com>
2025-02-14 23:18:06 +0700

    feat: api-account
    -Api get request with query parameter
    - pnpm format:write

---
+++ src/app/api/account/route.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const username = searchParams.get("username");
+    if (!username) {
+      return Response.json(
+        {
+          message: "failed",
+          error: "User does not exist.",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+    const account = await prisma.account.findUnique({
+      where: { username: username },
+    });
+    if (!account) {
+      return Response.json(
+        {
+          message: "failed",
+          error: "User does not exist.",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+    return Response.json({
+      message: "success",
+      data: {
+        name: account.username,
+        password: account.password,
+        role: account.role,
+      },
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        message: "failed",
+        error: error,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
