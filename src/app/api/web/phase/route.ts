dcdff8a49b2e9900b68f8d24d0cc56d29d62cace
098ff <chanatdakc@gmail.com>
2025-02-14 19:03:53 +0700

    style: pnpm format:write

--- src/app/api/web/phase/route.ts
+++ src/app/api/web/phase/route.ts
@@ -2,40 +2,40 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-    try {
-        const phase = await prisma.webPhase.findFirst();
+  try {
+    const phase = await prisma.webPhase.findFirst();
 
-        if (!phase) { // if phase === null value
-            return Response.json(
-                {
-                    message: "failed",
-                    error: "No phase data available.",
-                },
-                {
-                    status: 404,
-                },
-            );
-        }
+    if (!phase) {
+      // if phase === null value
+      return Response.json(
+        {
+          message: "failed",
+          error: "No phase data available.",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
 
-        return Response.json(
-            {
-                message: "success",
-                phase: "CAMP",
-            },
-            {
-                status: 200,
-            },
-        );
-    }
-    catch (error) {
-        return Response.json(
-            {
-                message: "failed",
-                error: error,
-            },
-            {
-                status: 500,
-            },
-        );
-    }
+    return Response.json(
+      {
+        message: "success",
+        phase: "CAMP",
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        message: "failed",
+        error: error,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 }
