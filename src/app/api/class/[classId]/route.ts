14f962f68bbfbe9a5108cdbb086d07326bdcaf7e
Yiwah <patsaporn.kaewkong@gmail.com>
2025-02-15 12:34:49 +0700

    refactor: err msg

--- src/app/api/class/[classId]/route.ts
+++ src/app/api/class/[classId]/route.ts
@@ -4,42 +4,41 @@ const prisma = new PrismaClient();
 
 export async function GET(
   req: Request,
-  { params }: { params: { classId: string } }
+  props: { params: Promise<{ classId: string }> },
 ) {
+  const params = await props.params;
   try {
-    const classId = params.classId;
-    console.log(classId);
     const classDetails = await prisma.class.findUnique({
-      where: { classId: params.classId},
+      where: { classId: params.classId },
     });
 
     if (!classDetails) {
-      return new Response(
-        JSON.stringify({
+      return Response.json(
+        {
           message: "failed",
           error: "Class does not exist.",
-        }),
-        { status: 404, headers: { "Content-Type": "application/json" } }
+        },
+        { status: 404 },
       );
     }
 
-    return new Response(
-      JSON.stringify({
+    return Response.json(
+      {
         message: "success",
-        data: { classDetails },
-      }),
-      { status: 200, headers: { "Content-Type": "application/json" } }
+        classDetails,
+      },
+      { status: 200 },
     );
   } catch (error) {
-    return new Response(
-      JSON.stringify({
+    return Response.json(
+      {
         message: "failed",
         error:
           error instanceof Error
             ? error.message
-            : "Failed to fetch class details.",
-      }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
+            : "Failed to fetch class details by classId.",
+      },
+      { status: 500 },
     );
   }
 }
