1f58698cf63fdbdbaa8e58d9cc6f8e29412cce71
Yiwah <patsaporn.kaewkong@gmail.com>
2025-02-17 14:17:58 +0700

    feat: api/staff/:staffId

---
+++ src/app/api/staff/[staffId]/route.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  req: Request,
+  props: { params: Promise<{ staffId: string }> },
+) {
+  const { staffId } = await props.params;
+  try {
+    const staffByStaffId = await prisma.staff.findUnique({
+      where: {
+        staffId: staffId,
+      },
+    });
+
+    if (!staffByStaffId) {
+      return new Response(
+        JSON.stringify({
+          message: "failed",
+          error: "Staff does not exist.",
+        }),
+        { status: 404, headers: { "Content-Type": "application/json" } },
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        message: "success",
+        staffByStaffId,
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: "failed",
+        error:
+          error instanceof Error
+            ? error.message
+            : "Failed to fetch staff by staffId.",
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
