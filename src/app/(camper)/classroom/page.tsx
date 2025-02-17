749fee0f0692f4b3bce0aba33fd4aa21af448dd7
GreenSRV <goodgreengreen111@gmail.com>
2025-02-17 18:44:41 +0700

    feat :([component] camperInfoTable)

--- src/app/(camper)/classroom/page.tsx
+++ src/app/(camper)/classroom/page.tsx
@@ -1,1 +1,39 @@
 // write your code here
+import CamperInfoTable from 'src/components/ui/info/camperInfoTable'
+import { number } from 'zod'
+
+export default function ClassroomPage() {
+  return (
+    <div>
+      <CamperInfoTable camper={[{ 
+      camperId: 1, 
+      name: "John", 
+      surname: 'Doe', 
+      nickname: 'Johnny', 
+      contactTel: '123456789', 
+      contactEmail: 'john.doe@example.com', 
+      parentTel: '987654321',
+      parentRelation: 'Mother',
+      school: 'XYZ School',
+      IDLine: 'johnny123',
+      FEyear: '2022',
+      room: 'Room B',
+      healthInfo: 'Peanut allergy',
+      foodInfo: 'Vegan',
+      },{camperId: 2, 
+      name: "Jane", 
+      surname: 'Smith', 
+      nickname: 'Janey', 
+      contactTel: '987654321', 
+      contactEmail: 'jane.smith@example.com', 
+      parentTel: '123456789',
+      parentRelation: 'Father',
+      school: 'LMN School',
+      IDLine: 'janey456',
+      FEyear: '2023',
+      room: 'Room C',
+      healthInfo: 'Lactose intolerant',
+      foodInfo: 'Gluten-free',}]}/>
+    </div>
+  )
+}
