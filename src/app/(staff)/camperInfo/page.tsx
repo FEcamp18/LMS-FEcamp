ba543d85b8f487c10d466d710e6f33edfc9ff792
GreenSRV <goodgreengreen111@gmail.com>
2025-02-26 18:20:35 +0700

    Change file

--- src/app/(staff)/camperInfo/page.tsx
+++ src/app/(staff)/camperInfo/page.tsx
@@ -1,6 +1,5 @@
 // write your code here
-import CamperInfoTable from 'src/components/ui/info/camperInfoTable'
-import { number } from 'zod'
+import CamperInfoTable from '../../../components/info/camperInfoTable'
 
 export default function ClassroomPage() {
   return (
