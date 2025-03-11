939224cb6b92aedeeab2c3c755aba225cacd983f
neennera <neennera@gmail.com>
2025-03-11 10:35:14 +0700

    add: get camper info

---
+++ src/app/api/camper/[camperId]/route.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  req: Request,
+  props: { params: Promise<{ camperId: string }> },
+) {
+  const { camperId } = await props.params;
+  try {
+    const camperBycamperId = await prisma.camper.findUnique({
+      where: {
+        camperId: camperId,
+      },
+    });
+
+    if (!camperBycamperId) {
+      return new Response(
+        JSON.stringify({
+          message: "failed",
+          error: "camperId does not exist.",
+        }),
+        { status: 404, headers: { "Content-Type": "application/json" } },
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        message: "success",
+        staff: camperBycamperId,
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: "failed",
+        error:
+          error instanceof Error
+            ? error.message
+            : "Failed to fetch staff by camperId.",
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
