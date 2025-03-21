0224f78b7e407d6d3ce9c0e8b06652925d7e8173
fahyada <fahyada@gmail.com>
2025-03-22 01:55:05 +0700

    feat: implement api anno subject-id

--- src/app/api/(web)/anno/[subjectId]/route.ts
+++ src/app/api/(web)/anno/[subjectId]/route.ts
@@ -50,3 +50,67 @@ export async function GET(
     await prisma.$disconnect();
   }
 }
+
+export async function POST(
+  req: Request,
+  props: { params: Promise<{ subjectId: string }> },
+) {
+  const { subjectId } = await props.params;
+  try {
+    const isSubjectIdExist = await prisma.subject.findUnique({
+      where: { subjectId },
+    });
+
+    if (!isSubjectIdExist) {
+      return new Response(
+        JSON.stringify({
+          message: "failed",
+          error: "SubjectId does not exist.",
+        }),
+        { status: 404, headers: { "Content-Type": "application/json" } },
+      );
+    }
+
+    const { annoTitle, annoText } = await req.json();
+
+    // Validate the input
+    if (!annoTitle || !annoText) {
+      return new Response(
+        JSON.stringify({
+          message: "failed",
+          error: "Announcement title and text are required.",
+        }),
+        { status: 400, headers: { "Content-Type": "application/json" } },
+      );
+    }
+
+    const newAnnouncement = await prisma.subjectAnnouncements.create({
+      data: {
+        subjectId,
+        annoTitle,
+        annoText,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: "success",
+        announcement: newAnnouncement,
+      }),
+      { status: 201, headers: { "Content-Type": "application/json" } },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: "failed",
+        error:
+          error instanceof Error
+            ? error.message
+            : "Failed to create new announcement.",
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } },
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
