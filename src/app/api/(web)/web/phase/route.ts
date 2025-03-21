93c42cf1256a13b78b74fcc2b802d49f26648dfd
fahyada <fahyada@gmail.com>
2025-03-22 02:50:46 +0700

    feat: implement patch api/web/phase

--- src/app/api/(web)/web/phase/route.ts
+++ src/app/api/(web)/web/phase/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, PHASE } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
@@ -38,4 +38,64 @@ export async function GET() {
       },
     );
   }
+}
+
+export async function PATCH(req: Request) {
+  try {
+    const userIsAdmin = checkIfAdmin(req);
+    if (!userIsAdmin) {
+      return Response.json(
+        {
+          message: "error",
+          error: "Access denied.",
+        },
+        {
+          status: 403,
+        },
+      );
+    }
+
+    const body = await req.json();
+    const phase: PHASE = body.phase;
+
+    if (!Object.values(PHASE).includes(phase)) {
+      return Response.json(
+        {
+          message: "failed",
+          error: "Invalid phase provided.",
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    await prisma.webPhase.update({
+      where: { phase: phase },
+      data: { phase },        
+    });
+
+    return Response.json(
+      {
+        message: "success",
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        message: "failed",
+        error: error,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+function checkIfAdmin(req: Request) {
+  return true; //assume user is admin
 }
