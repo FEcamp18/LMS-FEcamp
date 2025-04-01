6084c1c376870558c720913d645b9798ece926b9
Yiwah <patsaporn.kaewkong@gmail.com>
2025-04-01 12:31:53 +0700

    feat: patch password

--- src/app/api/(web)/account/route.ts
+++ src/app/api/(web)/account/route.ts
@@ -1,12 +1,46 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: Request) {
+interface UpdateAccountRequest {
+  username: string;
+  newPassword: string;
+}
+
+export async function PATCH(req: Request){
   try {
-    const { searchParams } = new URL(req.url);
-    const username = searchParams.get("username");
-    if (!username) {
+    const { username, newPassword } = (await req.json()) as UpdateAccountRequest;
+
+    if (!username || !newPassword) {
+      return Response.json(
+        {
+          message: "failed",
+          error: "Invalid username or password.",
+        },
+        {
+          status: 401,
+        },
+      );
+    }
+
+    if (newPassword.length < 8) {
+      return Response.json(
+        {
+          message: "failed",
+          error: "Password must be at least 8 characters.",
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    const user = await prisma.account.findUnique({
+      where: { username }
+    });
+
+    if (!user) {
       return Response.json(
         {
           message: "failed",
@@ -15,39 +49,35 @@ export async function GET(req: Request) {
         {
           status: 404,
         },
-      );
+      )
     }
-    const account = await prisma.account.findUnique({
-      where: { username: username },
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    await prisma.account.update({
+      where: { username },
+      data: { password: hashedPassword },
     });
-    if (!account) {
-      return Response.json(
-        {
-          message: "failed",
-          error: "username request not valid",
-        },
-        {
-          status: 404,
-        },
-      );
-    }
-    return Response.json({
-      message: "success",
-      data: {
-        name: account.username,
-        password: account.password,
-        role: account.role,
+    
+    return Response.json(
+      {
+        message: "success",
       },
-    });
+      {
+        status: 200,
+      },
+    )
   } catch (error) {
     return Response.json(
       {
         message: "failed",
-        error: error,
+        error: error instanceof Error ? error.message : "Internal Server Error",
       },
       {
         status: 500,
       },
-    );
+    )
+  } finally {
+    await prisma.$disconnect();
   }
 }
