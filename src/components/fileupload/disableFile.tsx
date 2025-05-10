60f5b0e2e84e9eea5c4b3fec8563a049a0e982e2
wnmay <wanathachat@gmail.com>
2025-05-10 22:38:25 +0700

    fix: fix disable function

--- src/components/fileupload/disableFile.tsx
+++ src/components/fileupload/disableFile.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { ROLE } from "@prisma/client";
 import axios from "axios";
-import { getSession } from "next-auth/react";
 
 interface ApiResponse {
   message: string;
@@ -9,16 +7,11 @@ interface ApiResponse {
 
 export async function disableFile(fileId: number) {
   try {
-    const session = await getSession();
-    const user = session?.user;
+    const response = await axios.patch<ApiResponse>(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/disable-file/${fileId}`,
+    );
 
-    if (!user || user.role !== (ROLE.STAFF || ROLE.BOARD)) {
-      const response = await axios.patch<ApiResponse>(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/disable-file/${fileId}`,
-      );
-
-      return response.data.message === "success";
-    }
+    return response.data.message === "success";
   } catch (error) {
     console.error("Failed to disable file:", error);
     return false;
