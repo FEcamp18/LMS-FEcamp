b9866a07ae71134a147a54463d1508dd431e4873
wnmay <wanathachat@gmail.com>
2025-05-10 17:11:17 +0700

    feat: styling upload file modal component

--- src/components/fileupload/uploadForm.tsx
+++ src/components/fileupload/uploadForm.tsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -16,19 +15,30 @@ import {
 import { Input } from "@/components/ui/input";
 import { useParams } from "next/navigation";
 import UploadFile from "./serverUploadFile";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import Image from "next/image";
 
 const formSchema = z.object({
-  file: z
-    .any()
-    .refine((file: FileList) => file?.length === 1, "File is required"),
+  file: z.any(),
   fileName: z.string().min(1, "File name is required"),
   fileDescription: z.string().min(1, "File description is required"),
   fileSubject: z.string(),
 });
 
 export default function UploadForm() {
+  const [open, setOpen] = useState(false);
   const params = useParams();
   const slug = params.subjectName as string;
+  const [isFileUploading, setIsFileUploading] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -39,13 +49,30 @@ export default function UploadForm() {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    //sent file data
-    const file = values.file[0];
-    if (!file) {
-      console.error("No file uploaded");
+  const handleFileUploading = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+    field: any,
+  ) => {
+    const files = e.target.files;
+    if (files?.length) {
+      setIsFileUploading(true);
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        field.onChange(files);
+      } finally {
+        setIsFileUploading(false);
+      }
     }
+  };
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
+      const file = values.file[0];
+      if (!file) {
+        console.error("No file uploaded");
+        return;
+      }
+
       const result = await UploadFile({
         file: file,
         fileName: values.fileName,
@@ -56,6 +83,7 @@ export default function UploadForm() {
       if (result.success) {
         console.log("File uploaded successfully:", result.fileInfo);
         form.reset();
+        setOpen(false);
       }
     } catch (error) {
       console.error("Upload failed:", error);
@@ -63,53 +91,198 @@ export default function UploadForm() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="file"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>PDF File</FormLabel>
-              <FormControl>
-                <Input
-                  type="file"
-                  accept="application/pdf"
-                  onChange={(e) => field.onChange(e.target.files)}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <button className="h-[40px] w-[158px] bg-light-gray text-white hover:bg-opacity-50">
+          เพิ่มไฟล์
+        </button>
+      </DialogTrigger>
+      <DialogContent className="h-[396px] w-[312px] rounded-none border-none bg-[url('/image/modal/background.png')] p-0 text-base [&>*]:rounded-none">
+        <Image
+          src="/image/modal/zigzag-top.png"
+          alt="top-edge"
+          width={312}
+          height={6}
+          className="absolute -top-[5px] left-0 w-full"
         />
-        <FormField
-          control={form.control}
-          name="fileName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>File name</FormLabel>
-              <FormControl>
-                <Input placeholder="File name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
+        <Image
+          src={"/image/modal/upload-top.png"}
+          alt="upload-top"
+          width={312}
+          height={100}
+          className="absolute -top-12 left-0"
         />
-        <FormField
-          control={form.control}
-          name="fileDescription"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>File description</FormLabel>
-              <FormControl>
-                <Input placeholder="File description" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
+        <Image
+          src="/image/modal/zigzag-cream.png"
+          alt="bottom-edge"
+          width={156}
+          height={6}
+          className="absolute -bottom-[6px] left-0"
         />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+        <Image
+          src="/image/modal/zigzag-gray.png"
+          alt="bottom-edge"
+          width={156}
+          height={6}
+          className="absolute -bottom-[6px] right-0"
+        />
+        <DialogHeader>
+          <DialogTitle className="mt-16 text-center text-2xl">
+            เพิ่มไฟล์
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-auto flex flex-col"
+          >
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        type="file"
+                        accept="application/pdf"
+                        onChange={(e) => handleFileUploading(e, field)}
+                        className={`absolute inset-0 w-full cursor-pointer opacity-0 ${
+                          field?.value?.[0]?.name || isFileUploading
+                            ? "pointer-events-none"
+                            : ""
+                        }`}
+                        id="file-upload"
+                        disabled={field?.value?.[0]?.name || isFileUploading}
+                      />
+                      <div
+                        className={`flex h-[56px] items-center justify-between gap-2 rounded-none border border-dark-brown px-4 ${
+                          field?.value?.[0]?.name || isFileUploading
+                            ? "bg-transparent text-black"
+                            : "bg-dark-gray text-white"
+                        }`}
+                      >
+                        <div className="flex items-center gap-2">
+                          <Image
+                            src={
+                              isFileUploading
+                                ? "/image/modal/clip-icon.png"
+                                : field?.value?.[0]?.name
+                                  ? "/image/modal/clip-icon.png"
+                                  : "/image/modal/file-icon.png"
+                            }
+                            alt={
+                              isFileUploading
+                                ? "uploading"
+                                : field?.value?.[0]?.name
+                                  ? "file-selected"
+                                  : "no-file"
+                            }
+                            height={
+                              field?.value?.[0]?.name || isFileUploading
+                                ? 24
+                                : 34
+                            }
+                            width={
+                              field?.value?.[0]?.name || isFileUploading
+                                ? 24
+                                : 34
+                            }
+                          />
+                          <label
+                            htmlFor="file-upload"
+                            className={
+                              field?.value?.[0]?.name || isFileUploading
+                                ? "cursor-default"
+                                : "cursor-pointer"
+                            }
+                          >
+                            {isFileUploading
+                              ? "อัพโหลดไฟล์..."
+                              : field.value?.[0]?.name
+                                ? field.value[0].name
+                                : "กดที่นี่เพื่อเพิ่มไฟล์"}
+                          </label>
+                        </div>
+                        {field.value?.[0]?.name && !isFileUploading && (
+                          <button
+                            type="button"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              field.onChange(null);
+                            }}
+                            className="flex items-center justify-center hover:opacity-80"
+                            aria-label="clear file selection"
+                          >
+                            <Image
+                              src="/image/modal/cancel-icon.png"
+                              alt="cancel-icon"
+                              width={24}
+                              height={24}
+                            />
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="fileName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="ชื่อ"
+                      {...field}
+                      className="h-[56px] placeholder-dark-gray"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="fileDescription"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="รายละเอียด"
+                      {...field}
+                      className="h-[104px] rounded-none border-y-[1px] border-dark-brown align-text-top placeholder:bg-dark-gray"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="mt-auto flex w-full p-0">
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => {
+                  form.reset();
+                  setOpen(false);
+                }}
+                className="h-[58px] flex-1 rounded-none border bg-transparent"
+              >
+                ยกเลิก
+              </Button>
+              <Button
+                type="submit"
+                className="h-[58px] flex-1 rounded-none border bg-dark-gray shadow-none"
+              >
+                เพิ่ม
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
