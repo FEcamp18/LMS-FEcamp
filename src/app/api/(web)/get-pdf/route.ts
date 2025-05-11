994b48d71c2e01dbc2954aedd00ef4aceac53c89
neennera <neennera@gmail.com>
2025-05-12 00:23:55 +0700

    chore: import type

--- src/app/api/(web)/get-pdf/route.ts
+++ src/app/api/(web)/get-pdf/route.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 import path from "path";
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
