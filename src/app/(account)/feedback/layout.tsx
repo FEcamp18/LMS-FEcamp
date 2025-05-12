09ce968c0fadb2e0d5024d614db95060bac5cc3e
neennera <neennera@gmail.com>
2025-05-12 23:43:02 +0700

    fix: fix font

--- src/app/(account)/feedback/layout.tsx
+++ src/app/(account)/feedback/layout.tsx
@@ -8,6 +8,20 @@ export const metadata: Metadata = {
   description: "Learning Management System for 18th FE Camp",
   icons: [{ rel: "icon", url: "/components/femainlogo.svg" }],
 };
+import { Prompt, Inknut_Antiqua } from "next/font/google";
+import "@/styles/globals.css";
+
+const prompt = Prompt({
+  subsets: ["latin"],
+  variable: "--font-prompt",
+  weight: ["400", "500", "600", "700"],
+});
+
+const inknut = Inknut_Antiqua({
+  subsets: ["latin"],
+  variable: "--font-inknut",
+  weight: ["400", "500", "600", "700"],
+});
 
 export default async function RootLayout({
   children,
--- src/app/(staff)/board/layout.tsx
+++ src/app/(staff)/board/layout.tsx
@@ -4,6 +4,21 @@ export const metadata: Metadata = {
   description: "Management System for 18th FE Camp",
 };
 
+import { Prompt, Inknut_Antiqua } from "next/font/google";
+import "@/styles/globals.css";
+
+const prompt = Prompt({
+  subsets: ["latin"],
+  variable: "--font-prompt",
+  weight: ["400", "500", "600", "700"],
+});
+
+const inknut = Inknut_Antiqua({
+  subsets: ["latin"],
+  variable: "--font-inknut",
+  weight: ["400", "500", "600", "700"],
+});
+
 export default function BoardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
