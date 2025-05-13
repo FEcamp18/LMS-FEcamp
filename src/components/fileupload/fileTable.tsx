ecd98f4783415e62e3b28152b6d6977b4c8a2731
wnmay <wanathachat@gmail.com>
2025-05-13 09:38:03 +0700

    refactor: relocate toaster component

--- src/components/fileupload/fileTable.tsx
+++ src/components/fileupload/fileTable.tsx
@@ -52,56 +52,58 @@ const FileTable = forwardRef<FileTableRef, { subjectId: string }>(
     };
 
     return (
-      <table className="min-w-full border-collapse rounded-lg border border-gray-300 shadow-md">
+      <>
         <Toaster />
-        <thead>
-          <tr className="bg-ameri text-white">
-            <th className="border border-gray-300 px-4 py-2">Filename</th>
-            <th className="border border-gray-300 px-4 py-2">Download</th>
-            {showDeleteButton && (
-              <th className="border border-gray-300 px-4 py-2">Delete</th>
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {files.length === 0 ? (
-            <tr>
-              <td
-                className="border border-gray-300 px-4 py-3 text-center text-gray-500"
-                colSpan={showDeleteButton ? 3 : 2}
-              >
-                No files available
-              </td>
+        <table className="min-w-full border-collapse rounded-lg border border-gray-300 shadow-md">
+          <thead>
+            <tr className="bg-ameri text-white">
+              <th className="border border-gray-300 px-4 py-2">Filename</th>
+              <th className="border border-gray-300 px-4 py-2">Download</th>
+              {showDeleteButton && (
+                <th className="border border-gray-300 px-4 py-2">Delete</th>
+              )}
             </tr>
-          ) : (
-            files.map((file) => (
-              <tr key={file.fileId} className="bg-gray-200 even:bg-gray-100">
-                <td className="border border-gray-300 px-4 py-3">
-                  {file.fileTitle}
+          </thead>
+          <tbody>
+            {files.length === 0 ? (
+              <tr>
+                <td
+                  className="border border-gray-300 px-4 py-3 text-center text-gray-500"
+                  colSpan={showDeleteButton ? 3 : 2}
+                >
+                  No files available
                 </td>
-                <td className="border border-gray-300 px-4 py-3 text-center">
-                  <button
-                    className="cursor-pointer rounded-md bg-green-500 px-3 py-1 text-white transition-colors duration-200 hover:bg-green-600"
-                    onClick={() => getFile(file.fileId, file.fileTitle)}
-                  >
-                    Download
-                  </button>
-                </td>
-                {showDeleteButton && (
+              </tr>
+            ) : (
+              files.map((file) => (
+                <tr key={file.fileId} className="bg-gray-200 even:bg-gray-100">
+                  <td className="border border-gray-300 px-4 py-3">
+                    {file.fileTitle}
+                  </td>
                   <td className="border border-gray-300 px-4 py-3 text-center">
                     <button
-                      className="cursor-pointer rounded-md bg-red-500 px-3 py-1 text-white transition-colors duration-200 hover:bg-red-600"
-                      onClick={() => handleDelete(file.fileId)}
+                      className="cursor-pointer rounded-md bg-green-500 px-3 py-1 text-white transition-colors duration-200 hover:bg-green-600"
+                      onClick={() => getFile(file.fileId, file.fileTitle)}
                     >
-                      Delete
+                      Download
                     </button>
                   </td>
-                )}
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+                  {showDeleteButton && (
+                    <td className="border border-gray-300 px-4 py-3 text-center">
+                      <button
+                        className="cursor-pointer rounded-md bg-red-500 px-3 py-1 text-white transition-colors duration-200 hover:bg-red-600"
+                        onClick={() => handleDelete(file.fileId)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  )}
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </>
     );
   },
 );
