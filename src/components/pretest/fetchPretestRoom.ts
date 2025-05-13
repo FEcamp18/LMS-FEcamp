ade2bcd5b5529431c3340fbd0b26795dc110c3ad
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2025-05-13 16:33:08 +0700

    style: pnpm format write

--- src/components/pretest/fetchPretestRoom.ts
+++ src/components/pretest/fetchPretestRoom.ts
@@ -5,32 +5,32 @@ const cleanPretestRoom = (
     (acc, camper) => {
       // Initialize the array for this location if it doesn't exist
       if (!acc[camper?.examLocation]) {
-        acc[camper.examLocation] = []
+        acc[camper.examLocation] = [];
       }
 
       // Add the camper to the array for their exam location
       if (camper?.examLocation) {
-        acc[camper?.examLocation]?.push(camper)
+        acc[camper?.examLocation]?.push(camper);
       }
 
-      return acc
+      return acc;
     },
     {} as Record<string, ExamDataInterface[]>,
-  )
+  );
 
-  return groupedData
-}
+  return groupedData;
+};
 
-export default cleanPretestRoom
+export default cleanPretestRoom;
 
 export interface ExamDataInterface {
   camper: {
-    name: string
-    surname: string
-    nickname: string
-  }
-  camperId: string
-  camperName: string
-  seatNumber: string
-  examLocation: string
+    name: string;
+    surname: string;
+    nickname: string;
+  };
+  camperId: string;
+  camperName: string;
+  seatNumber: string;
+  examLocation: string;
 }
