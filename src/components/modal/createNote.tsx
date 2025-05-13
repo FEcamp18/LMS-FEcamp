a7e7595ee66d57b879b47b19b1f9cccf6b847a0e
neennera <neennera@gmail.com>
2025-05-14 01:02:03 +0700

    feat: camperinfo post new note

--- src/components/modal/createNote.tsx
+++ src/components/modal/createNote.tsx
@@ -21,20 +21,52 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
 
 const formSchema = z.object({
   noteDescription: z.string().min(1, "Description is required"),
 });
 
-export default function CreateNote() {
+export default function CreateNote({
+  camperId,
+  staffId,
+}: {
+  camperId: string;
+  staffId: string;
+}) {
   const [open, setOpen] = useState(false);
-  const [description, setDescription] = useState("");
 
-  async function onSubmit() {
-    //write function here
-    console.log(description);
-    form.reset();
-    setOpen(false);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const response = await fetch(`/api/staff/notes`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "staff-id": staffId,
+        },
+        body: JSON.stringify({
+          camperId: camperId,
+          content: values.noteDescription,
+          type: "NORMAL",
+        }),
+      });
+
+      const data = (await response.json()) as {
+        message: string;
+        error?: string;
+      };
+
+      if (response.ok && data.message === "success") {
+        toast.success("หมายเหตุเพิ่มสำเร็จ! กรุณารีเฟรชหน้าจอ");
+        form.reset();
+        setOpen(false);
+      } else {
+        toast.error(data.error ?? "เกิดข้อผิดพลาดในการเพิ่มหมายเหตุ");
+      }
+    } catch (error) {
+      console.error("Error creating note:", error);
+      toast.error("เกิดข้อผิดพลาดในการเพิ่มหมายเหตุ");
+    }
   }
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -46,9 +78,13 @@ export default function CreateNote() {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
+      <div className="text-base">
+        <Toaster />
+      </div>
+
       <DialogTrigger asChild>
-        <button className="h-[40px] w-[158px] bg-light-gray text-white hover:bg-opacity-50">
-          สร้างโน๊ต
+        <button className="h-[40px] w-[158px] bg-light-gray text-base text-white hover:bg-opacity-50">
+          + โน๊ต
         </button>
       </DialogTrigger>
       <DialogContent className="h-[332px] w-[312px] rounded-none border-none bg-[url('/image/modal/background.webp')] p-0 text-base">
@@ -103,6 +139,22 @@ export default function CreateNote() {
                     <FormControl>
                       <Input
                         placeholder="รายละเอียด"
+                        {...field} // Use react-hook-form's field object
+                        className="h-auto rounded-none border-t border-dark-brown align-text-top placeholder:bg-dark-gray"
+                      />
+                    </FormControl>
+                    <FormMessage className="px-2" />
+                  </FormItem>
+                )}
+              />
+              {/* <FormField
+                control={form.control}
+                name="noteDescription"
+                render={() => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="รายละเอียด"
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
                         className="h-auto rounded-none border-t border-dark-brown align-text-top placeholder:bg-dark-gray"
@@ -111,7 +163,7 @@ export default function CreateNote() {
                     <FormMessage className="px-2" />
                   </FormItem>
                 )}
-              />
+              /> */}
               <div className="flex w-full">
                 <Button
                   type="button"
