9df128238ff6d5fa4055cc3ca4725e452cb89470
Thukdanai Thaothawin <arkaza2547@gmail.com>
2025-05-16 12:11:50 +0700

    chore:element provided

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,22 +23,37 @@ export default function Home() {
     setLoad(false)
   }, [])
 
-  if (load || webPhase === "") return <>dev อย่าลืมเปิด docker</>
+  if (load || webPhase === "") return <div>Loading</div>
   if (webPhase === "CLOSED") {
     return <LandingClose />
   } else if (webPhase === "BEFORE_CAMP") {
     return <LandingPreCamp />
-  } else if (webPhase === "ARCHIVE") {
-    return <LandingPreClose />
-  } else {
+  } 
+  else if (webPhase === "ARCHIVE") {
+    return <LandingArchive />
+  } 
+  else {
     return <LandingCamp />
   }
 }
 
 function LandingClose() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center pt-16">
-      <div className="text-4xl font-bold">เว็บไซต์กำลังปิดปรับปรุง</div>
+    <div className="relative w-screen h-screen bg-[url('/image/background/landingpage_background.webp')] bg-cover bg-center bg-no-repeat bg-contain">
+       <div className="absolute inset-0 bg-white/10 " />
+      <div className="flex h-full w-full flex-col items-center justify-center pt-16 backdrop-brightness-75">
+         <img src="/logo.svg" alt="Logo" className="absolute top-0 w-72 h-72 mb-6" />
+        <div className="text-4xl font-bold text-white text-center px-4">
+          ตอนนี้วิหารได้ปิดลงแล้ว<br />
+          ร่อยรอยความทรงจำของเรายังคงอยู่เสมอ
+        </div>
+        <Link
+          href="/login"
+          className="mt-6 rounded-full bg-blue-500 px-6 py-3 text-white hover:bg-blue-400"
+        >
+          เข้าสู่ระบบ
+        </Link>
+      </div>
     </div>
   )
 }
@@ -53,7 +68,7 @@ function LandingPreCamp() {
   )
 }
 
-function LandingPreClose() {
+function LandingArchive() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center pt-16">
       <div className="text-center">
